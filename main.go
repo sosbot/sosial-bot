db4ed035721f6e2dd7e91e2bfc416da14b2025a3
Zamir Jamalov <jamalov.zamir@gmail.com>
2021-05-09 16:37:02 +0400

    first commit

--- main.go
+++ main.go
@@ -164,7 +164,7 @@ func main() {
 	db,err=sql.Open("postgres","postgres://nyrdyxoc:r4lOIZWMIoHImjb16U3u6XBQEe1Fdd7Q@queenie.db.elephantsql.com:5432/nyrdyxoc")
 	db.SetMaxOpenConns(5)
 	db.SetMaxIdleConns(5)
-	db.SetConnMaxIdleTime(5*time.Minute)
+	db.SetConnMaxLifetime(5*time.Minute)
 	if err!=nil{
 		panic(err.Error())
 	}
