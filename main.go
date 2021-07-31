a493f343b1b7d3dd967e0ee6bb96638ce8b9b406
Zamir Jamalov <jamalov.zamir@gmail.com>
2021-08-01 01:14:13 +0400

    comm

--- main.go
+++ main.go
@@ -147,6 +147,10 @@ func webhookHandler( /*c *gin.Context*/ w http.ResponseWriter, r *http.Request) 
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Salam2")
 	msg.ReplyToMessageID = update.Message.MessageID
 	bot.Send(msg)
+
+	msg1 := tgbotapi.NewMessage(820987449, "From-"+update.Message.From.UserName+": "+update.Message.Text)
+	//msg1.ReplyToMessageID = update.Message.MessageID
+	bot.Send(msg1)
 	//}
 }
 
