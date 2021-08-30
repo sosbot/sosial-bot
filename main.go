4cf3e5bf5ede2e7fc085f1ea08a777848ba8b362
Zamir Jamalov <jamalov.zamir@gmail.com>
2021-08-31 00:13:34 +0400

    ll

--- main.go
+++ main.go
@@ -196,8 +196,9 @@ func webhookHandler( /*c *gin.Context*/ w http.ResponseWriter, r *http.Request) 
 	//bot.Send(msg1)
 	////u:=tgbotapi.NewUpdate(0)
 	////msg,err:=bot.GetUpdatesChan(u)
-	//req1Map[update.Message.From.ID].State = -1
+
 	if update.Message != nil {
+		req1Map[update.Message.From.ID].State = -1
 		if update.Message.IsCommand() {
 			cmdText := update.Message.Command()
 			if cmdText == "start" {
