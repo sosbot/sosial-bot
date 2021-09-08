25d75b6cdb32fedd6f3992beb6642093fec32380
Zamir Jamalov <jamalov.zamir@gmail.com>
2021-09-08 22:03:27 +0400

    f

--- main.go
+++ main.go
@@ -337,6 +337,7 @@ func webhookHandler( /*c *gin.Context*/ w http.ResponseWriter, r *http.Request) 
 				msg.ReplyMarkup = mainMenu
 				bot.Send(msg)
 			}
+			back_clicked_once = true
 			if update.Message.Text == mainMenu.Keyboard[1][0].Text { //"🏠 Müraciət ünvanı":
 				cmdLine = mainMenu.Keyboard[1][0].Text
 				cmdLineMenu = "mainMenu"
