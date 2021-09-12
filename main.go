608984247b41d4dd47749f718acd9c84fda2a5a5
Zamir Jamalov <jamalov.zamir@gmail.com>
2021-09-12 15:15:43 +0400

    fwe

--- main.go
+++ main.go
@@ -264,7 +264,7 @@ func webhookHandler( /*c *gin.Context*/ w http.ResponseWriter, r *http.Request) 
 			if update.Message.Text == mainMenu.Keyboard[0][1].Text {
 				cmdLine = mainMenu.Keyboard[0][1].Text
 				cmdLineMenu = "mainMenu"
-				rows, err := db.Query("SELECT reqnumber,reqtype,reqtext FROM public.requests WHERE reqfrom = :1", 820987449)
+				rows, err := db.Query("SELECT reqnumber,reqtype,reqtext FROM public.requests WHERE reqfrom = " + strconv.Itoa(update.Message.From.ID))
 				if err != nil {
 					log.Println(err)
 				}
