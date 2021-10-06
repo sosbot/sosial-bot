9a8f836f82f1ba368ba01d8684ef7b3dd4aa970a
Zamir Jamalov <jamalov.zamir@gmail.com>
2021-10-07 00:52:52 +0400

    a

--- main.go
+++ main.go
@@ -240,6 +240,8 @@ func webhookHandler( /*c *gin.Context*/ w http.ResponseWriter, r *http.Request) 
 		if 1 == 1 {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "SosialBot-un funksionallığını daha da yaxşılaşdırmaq məqsədilə komanda olaraq, gecə-gündüz işləyirik. Hal-hazırda yeni dəyişikliklərimizi tətbiq etməyə çalışırıq. Bu səbəbdən botun funksionallığını müvəqqəti olaraq dayandırmışıq. Az sonra, son yeniliklərlə, bot fəaliyyətini davam etidərəcək. Anlayışınız üçün təşəkkür edirik.")
 			bot.Send(msg)
+			msg1 := tgbotapi.NewPhotoShare(update.Message.Chat.ID, `https://telegram.org.ru/uploads/posts/2018-05/1525167580_photo_2018-04-07_12-02-10.jpg`)
+			bot.Send(msg1)
 			return
 		}
 		if update.Message.IsCommand() {
