2b2a5ec7e5716af87f88e565aadddfc19b80fb7b
Zamir Jamalov <jamalov.zamir@gmail.com>
2021-10-10 14:21:15 +0400

    a

--- main.go
+++ main.go
@@ -536,7 +536,7 @@ func webhookHandler( /*c *gin.Context*/ w http.ResponseWriter, r *http.Request) 
 func execQuestions(QuestionTypeName string) {
 	logger(123, QuestionTypeName, LogAppInfo)
 
-	rows, err := db.Query("SELECT qt.name,q.state,q.request_text,q.request_error_text,q.response_validation_type from public.questions q,public.question_type qt  where qt.id=q.question_type_id and qt.name='Haqqı ödənilən ictimai işlərə cəlb olunma' ")
+	rows, err := db.Query("SELECT qt.name,q.state,q.request_text,q.request_error_text,q.response_validation_type from public.questions q,public.question_type qt  where qt.id=q.question_type_id and qt.name= " + QuestionTypeName)
 	if err != nil {
 		log.Println(err)
 	}
@@ -547,6 +547,7 @@ func execQuestions(QuestionTypeName string) {
 	var requestText string
 	var requestErrorText string
 	var responseValidationType string
+	logger(123, "ok1", LogAppInfo)
 	for rows.Next() {
 
 		sequence++
@@ -559,6 +560,7 @@ func execQuestions(QuestionTypeName string) {
 		questionsArrMap[sequence].ResponseValidationType = responseValidationType
 
 	}
+	logger(123, "ok2", LogAppInfo)
 	logger(123, strconv.Itoa(sequence), LogAppInfo)
 
 }
