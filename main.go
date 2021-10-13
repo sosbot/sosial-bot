451054e7d3ad7bc78bda1600de1454c5a695a5e6
Zamir Jamalov <jamalov.zamir@gmail.com>
2021-10-13 19:15:33 +0400

    a

--- main.go
+++ main.go
@@ -346,7 +346,7 @@ func webhookHandler( /*c *gin.Context*/ w http.ResponseWriter, r *http.Request) 
 					var requestNumber string
 					var status string
 					_ = rows.Scan(&questionTypeName, &answer, &requestDate, &requestNumber, &status)
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "<u><b>Sorğu nömrəsi:</b></u>"+"\n"+requestNumber+"\n"+"<u><b>Müraciət mövzusu:</u></b>  "+"\n"+questionTypeName+"\n"+"<u><b>Sorğu və Cavab:</u></b>"+"\n"+answer+"\n"+"<u><b>Müraciət Tarixi:</u></b>"+"\n"+requestDate+"\n"+"<u><b>Müraciətin statusu:</u></b>"+"\n"+status)
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "<u><b>Sorğu nömrəsi:</u></b>"+"\n"+requestNumber+"\n"+"<u><b>Müraciət mövzusu:</u></b>  "+"\n"+questionTypeName+"\n"+"<u><b>Sorğu və Cavab:</u></b>"+"\n"+answer+"\n"+"<u><b>Müraciət Tarixi:</u></b>"+"\n"+requestDate+"\n"+"<u><b>Müraciətin statusu:</u></b>"+"\n"+status)
 					msg.ParseMode = "html"
 					msg.ReplyMarkup = mainMenu
 					bot.Send(msg)
