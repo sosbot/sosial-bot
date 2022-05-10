f7069f6acb53aba6a110343a67000271a66d318e
ZAMIR JAMALOV <jamalov.zamir@gmail.com>
2022-05-10 23:56:33 +0400

    added audio send

--- main.go
+++ main.go
@@ -329,7 +329,7 @@ func webhookHandler( /*c *gin.Context*/ w http.ResponseWriter, r *http.Request) 
 
 			//vc, _ := ioutil.ReadAll(r.Body)
 			sqlStatement := `insert into voices(voice) values($1)`
-			_, err := db.Exec(sqlStatement, msg.File)
+			_, err := db.Exec(sqlStatement, msg.BaseFile.File)
 			if err != nil {
 				panic(err)
 			}
