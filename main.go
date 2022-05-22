1565fe7771d98a878d934711f7e4f1a284c10a62
ZAMIR JAMALOV <jamalov.zamir@gmail.com>
2022-05-22 12:19:23 +0400

    voice table altered

--- main.go
+++ main.go
@@ -1114,7 +1114,7 @@ func messagesGetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func queryUserRepos(repos *repositoryUsers) error {
-	rows, err := db.Query(`select sentby,coalesce((select count(*) from messages where sentby=m.sentby and viewedat is null group by m.sentby),0) as cnt from messages m where sentby is not null group by sentby`)
+	rows, err := db.Query(`select sentby,cnt from (select sentby,coalesce((select count(*) from messages where sentby=m.sentby and viewedat is null group by m.sentby),0) as cnt from messages m where sentby is not null group by sentby) order by cnt desc`)
 
 	if err != nil {
 		return err
