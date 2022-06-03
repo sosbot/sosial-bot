e288ded78476f6aa70a82cf0bee23348d123699f
ZAMIR JAMALOV <jamalov.zamir@gmail.com>
2022-06-03 10:35:02 +0400

    voice table altered

--- template.go
+++ template.go
@@ -10,10 +10,10 @@ type ViewData struct {
 	Message string
 }
 
-var tplInputTemplate = `<div class="col-12 col-md-1" ><label for="%s">%s</label> <input type="text" class="form-control form-control-lg" id="%s" name="%s" required size="%s" placeholder="%s" minlength="%s" maxlength="%s" title="%s"></div><br>`
-var tplDateTemplate = `<div class="col-12 col-md-1"><label for="%s">%s</label><input type="date"  class="form-control form-control-lg" id="%s" name="%s" value="" min="1900-01-01" max="2030-12-31"></div><br>`
-var tplSelectTemplate = `<div class="col-12 col-md-1"><label for="%s">%s</label><select class="form-control-lg"  name="%s" id="%s">%s</select></div><br>`
-var tplCheckboxTemplate = `<div class="col-12 col-md-1"><input  class="form-control-lg""  type="checkbox" id="%s" name="%s" value=%s><label for="%s">%s</label></div><br>`
+var tplInputTemplate = `<div class="col-xs-12" ><label for="%s">%s</label> <input type="text" class="form-control form-control-lg" id="%s" name="%s" required size="%s" placeholder="%s" minlength="%s" maxlength="%s" title="%s"></div><br>`
+var tplDateTemplate = `<div class="col-xs-12"><label for="%s">%s</label><input type="date"  class="form-control form-control-lg" id="%s" name="%s" value="" min="1900-01-01" max="2030-12-31"></div><br>`
+var tplSelectTemplate = `<div class="col-xs-12"><label for="%s">%s</label><select class="form-control-lg"  name="%s" id="%s">%s</select></div><br>`
+var tplCheckboxTemplate = `<div class="col-xs-12"><input  class="form-control-lg""  type="checkbox" id="%s" name="%s" value=%s><label for="%s">%s</label></div><br>`
 
 type inputForm struct {
 	Fields []string
